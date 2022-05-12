9a83dc67d589d750fbe7a70d882f81b4973fdc04
shashwatsingh3103 <104979175+shashwatsingh3103@users.noreply.github.com>
2022-05-12 15:02:21 +0530

    efheh
    fef

---
+++ 4b.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,h,v;
+	printf("enter the value of l");
+	scanf("%d",&l);
+	printf("enter the value of b");
+	scanf("%d",&b);
+	printf("enter the value of h");
+	scanf("%d",&h);
+	v=l*b*h;
+	printf("volume =%d",v);
+	return 0;
+}
